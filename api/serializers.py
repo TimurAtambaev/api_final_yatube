e2df9bdac49d2f64b411c8df129e0971c6e078c5
TimurAtambaev <atambaev86@gmail.com>
2021-04-08 19:31:36 +0300

    "api_final_yatube"

--- api/serializers.py
+++ api/serializers.py
@@ -1,19 +1,54 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
-from .models import Comment, Post
+from .models import Comment, Follow, Group, Post
 
-
-class PostSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
-
-    class Meta:
-        fields = ('id', 'text', 'author', 'pub_date')
-        model = Post
+User = get_user_model()
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
+    author = serializers.SlugRelatedField(read_only=True,
+                                          slug_field='username')
 
     class Meta:
-        fields = ('id', 'author', 'post', 'text', 'created')
         model = Comment
+        fields = '__all__'
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['title']
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        queryset=User.objects.all(),
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(queryset=User.objects.all(),
+                                             slug_field='username')
+
+    class Meta:
+        model = Follow
+        fields = ['following', 'user']
+        validators = [UniqueTogetherValidator(queryset=Follow.objects.all(),
+                                              fields=['following', 'user'])]
+
+    def validate(self, data):
+        if (self.context['request'].method == 'POST'
+                and data['user'] == data['following']):
+            raise serializers.ValidationError('Нельзя подписаться '
+                                              'на самого себя')
+        return data
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(read_only=True,
+                                          slug_field='username')
+
+    class Meta:
+        model = Post
+        fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,1 +1,60 @@
-# TODO:  Напишите свой вариант
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import permissions, viewsets, filters
+
+from .models import Group, Follow, Post
+from .permissions import IsAuthorOrReadOnly
+from .serializers import (CommentSerializer, GroupSerializer, FollowSerializer,
+                          PostSerializer)
+
+User = get_user_model()
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsAuthorOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['group']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsAuthorOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['id']
+
+    def get_queryset(self):
+        self.post = get_object_or_404(Post, id=self.kwargs.get('id'))
+        return self.post.comments
+
+    def perform_create(self, serializer):
+        get_object_or_404(Post, id=self.kwargs.get('id'))
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsAuthorOrReadOnly]
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['user__username', 'following__username']
+
+    def get_queryset(self):
+        return self.request.user.following
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
